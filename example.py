51585790bee840cda6f79381bada4b446cc4476f
Steven Conaway <Steven.conaway@icloud.com>
2018-12-31 16:52:32 -0800

    Same.

--- example.py
+++ example.py
@@ -3,9 +3,11 @@
 import WazeRouteCalculator
 
 from_address = 'Budapest, Hungary'
-to_address = 'Győr, Hungary'
-route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address)
+to_address = 'Gyor, Hungary'
+route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address, 'EU')#, 'US')
+
+
 try:
     route.calc_route_info()
 except WazeRouteCalculator.WRCError as err:
-    print err
+    print(err)
